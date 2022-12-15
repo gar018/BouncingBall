13627d86f78a56e822a3f25d2207b2dd16e06245
gar018 <119434207+gar018@users.noreply.github.com>
2022-12-14 22:00:33 -0500

    ACTUALLY ACTUALLLYYYY sketch.js

--- sketch.js
+++ sketch.js
@@ -14,6 +14,15 @@
  *    If so, the ball reverses the move direction.
  */
 
+/*--------------------------------------------------------------------------
+Gordon Rose - 12/14/22
+Three "Meaningful Changes" to the Program:
+1. 'Hello World' box & collision are centered on screen.
+2. Fixed Box collision that traps the balls.
+3. Made the box color change each time it is collided with.
+---------------------------------------------------------------------------
+*/
+
 const BOX_WIDTH  = 200;  // textbox dimensions
 const BOX_HEIGHT = 100;
 
@@ -29,7 +38,9 @@ function preload() {
 function setup() {
 
 //  createCanvas(windowWidth, windowHeight);
-  createCanvas(600,400)
+  var windowWidth = 600;
+  var windowHeight = 400;
+  createCanvas(windowWidth, windowHeight);
 
   
   noStroke();
@@ -37,10 +48,14 @@ function setup() {
   //sound.play();    // play the audio file once
   sound.loop();  // play the sound file repeatedly
   
+  createBox();
+  
   for (var ballNum = 0; ballNum < 10; ballNum++) {
   	balls[ballNum] = new Ball();  
   }
 
+  
+  
   let y = height;
   testBall = new Ball();
   testBall.size = 50;
@@ -55,21 +70,49 @@ function setup() {
 
 function createBox() {
   // prepare a box first
-  strokeWeight(4);
-  rect(0, 0, BOX_WIDTH, BOX_HEIGHT);
   
-  textSize(32);           // size of the text (pixels)
-  fill(0, 102, 153);      // fill() takes R,G,B values as the color
-  // draw the text in the box (x,y,width,height) with the color in fill()
-  textAlign(CENTER);
-  text('Hello World!', BOX_WIDTH/2, BOX_HEIGHT/2);   
+  helloBox = new Box();
  
 }
 
+class Box {
+  
+  constructor() {
+    strokeWeight(4);
+    this.boxX=300;
+    this.boxY=200;
+    this.boxWidth = BOX_WIDTH;
+    this.boxHeight = BOX_HEIGHT;
+    
+    this.red=random(255);
+    this.green=random(255);
+    this.blue=random(255);
+    this.alpha=20;
+    
+    
+  }
+  
+  changeColor(r,g,b,a) {
+    this.red=r;
+    this.green=g;
+    this.blue=b;
+    this.alpha=a;
+  }
+  
+  display() {
+    fill(this.red,this.green,this.blue,this.alpha);
+    rectMode(CENTER);
+    rect( this.boxX, this.boxY, BOX_WIDTH, BOX_HEIGHT);
+    fill(0, 102, 153);
+    textAlign(CENTER);
+    textSize(32);
+    text('Hello World!', this.boxX, this.boxY, this.boxWidth, this.boxHeight-60);
+  }
+}
 function draw() {
 
   background(255);
-  createBox();
+  
   
   testBallMove();  // a special ball to test corner collision
   
@@ -78,6 +121,8 @@ function draw() {
     balls[ballNum].checkForHitWall();
     balls[ballNum].checkForHitBox();
     balls[ballNum].moveBall();
+    
+    helloBox.display();
     
     if (mouseIsPressed) {
       balls[ballNum].randomize()
@@ -113,6 +158,9 @@ class Ball { // Constructor
     this.red   = random(255);
     this.green = random(255);
     this.blue  = random(255)
+    
+    // how many net collisions between the wall and the box this ball has had
+    this.netCollisions = 0;
   }
   
   display() {
@@ -129,21 +177,34 @@ class Ball { // Constructor
   
     let radius = this.size / 2;
     if ((this.ballY+radius) > height || (this.ballY-radius) < 0) {
-  	  this.speedY = -this.speedY;  
+  	  this.speedY = -this.speedY;
+      this.netCollisions += 1;
+      this.escape();
   	}
     if ((this.ballX+radius) > width  || (this.ballX-radius) < 0) {
-      this.speedX = -this.speedX;  
+      this.speedX = -this.speedX; 
+      this.netCollisions += 1;
+      this.escape();
     }
   }
   
   checkForHitBox() {
     
     let radius = this.size / 2;
+    
+    let xCollisionMax = 400;
+    let xCollisionMin = 200;
+    let yCollisionMax = 250;
+    let yCollisionMin = 150;
 
 //    if (((this.ballX-radius) < BOX_WIDTH && (this.ballY-radius) < BOX_HEIGHT) || d < radius) {
-    if (((this.ballX-radius) < BOX_WIDTH && (this.ballY-radius) < BOX_HEIGHT)) {
+    if (((this.ballX) < xCollisionMax && (this.ballX) > xCollisionMin) && ((this.ballY) < yCollisionMax && (this.ballY) > yCollisionMin)) {
       // bump into the textbox, need to reverse direction
       this.reverseBall();
+      helloBox.changeColor(this.red,this.green,this.blue,20);
+      this.netCollisions -= 1;
+      this.escape();
+      //change color of box to color of box
     }
   }
   
@@ -159,4 +220,13 @@ class Ball { // Constructor
   	this.ballY += this.speedY;
   }
   
+  escape() {
+    if (Math.abs(this.netCollisions) > 48) {
+      this.ballX = random(100, width)
+      this.ballY = random(100, height)
+      this.netCollisions = 0;
+    }
+    
+  }
+    
 }
